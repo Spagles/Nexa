5139214049f83525c4d10aadec9c9aa668758059
StormCode <william.jonathan.us@gmail.com>
2026-07-31 21:55:18 -0700

    Prep for compile.

--- dTs/compile.bat
+++ dTs/compile.bat
@@ -6,7 +6,7 @@ python -m nuitka ^
   --product-name="Nexa" ^
   --file-version=0.2.2.0 ^
   --product-version=0.2.2.0 ^
-  --file-description="Nexa - Minecraft Server Orchestration Software" ^
+  --file-description="Nexa" ^
   --copyright="Copyright (c) 2026 StormCode & Contributors" ^
   --assume-yes-for-downloads ^
   --follow-imports ^
